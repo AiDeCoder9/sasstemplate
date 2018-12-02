4c9c8e06774f8887103b53d45da5469100884d15
ArchaiveArthus <35091588+ArchaiveArthus@users.noreply.github.com>
2018-12-02 16:52:41 +0545

    addded color theme with custom data attribute

--- js/custom.js
+++ js/custom.js
@@ -1,46 +1,25 @@
-$('.mynavbar-search-icon').click(function(){
-  $('.mynavbar-search-icon').addClass('text-primary');
-  $('.mynavbar-search-input').toggle().addClass('d-block');
-});
-$('.mynavbar-activator').click(function(){
-  $('.mynavbar-back').addClass('d-block');
-});
-$('.mynavbar-back-close').click(function(){
-  $('.mynavbar-back').removeClass('d-block');
-});
+
 
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
 
-$(function() {
-    // Smooth Scrolling
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
-      }
-    });
+$('#theme-active').hover(
+  function(){
+  $('.theme-setter').addClass('d-block');
+}
+);
 
-    $('.nav-activator').click(function(){
-      $('.nav-side').addClass('d-block');
-    });
-    $('#nav-dismiss').click(function(){
-      $('.nav-side').removeClass('d-block');
-    });
-  });
+$('.theme-setter').hover(function(){
+  $('.theme-setter').addClass('d-block');
+},function(){
+  $('.theme-setter').removeClass('d-block');
+});
+$('.bar-task-dropdown').mouseleave(function(){
+  $('.theme-setter').removeClass('d-block');
+})
 
 
-  window.onscroll = function() {myFunction()};
-
-var nav = document.getElementById("navbar-example2");
-var sticky = nav.offsetTop;
 
 
 
